b917cbe2cb4802d7857a97b43b5c8180aa52b45a
omrajsharma <omraj7413@gmail.com>
2023-07-07 09:04:13 +0530

    refactor: sort latest ads

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,6 +14,7 @@ function Home() {
       for (let dataItem in data) {
         productList.push({...data[dataItem], productId: dataItem})
       }
+      productList.reverse();
       setProducts(productList)
     })
   }, [])
